fca22c45cb648f80528d3bf595b6472b7c8e3283
liu kelin <314566990@qq.com>
2016-04-08 10:52:20 +0800

    使用POP3获取邮箱邮件内容

---
+++ get_mail.py
@@ -0,0 +1,189 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# get Mail
+# 打开邮箱并判断邮件获取邮件内容
+# @author: liukelin  314566990@qq.com
+# http://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/0014320098721191b70a2cf7b5441deb01595edd8147196000
+# 
+import os
+from email.parser import Parser
+from email.header import decode_header
+from email.utils import parseaddr
+import poplib
+
+dir_ = os.getcwd()
+
+pops = {'126.com':'pop.126.com','163.com':'pop.163.com','qq.com':'pop.qq.com', 'sina.com':'pop.sina.com'}
+
+#
+# 获取邮件内容
+# limit 获取最近邮件数量
+#
+def get_mail(email, password, limit=1):
+	pop3_server = ''
+	st = email.split('@')[1]
+	if st and (st in pops):
+		pop3_server = pops[st]
+
+	msgAll = []
+	try:
+		# 输入邮件地址, 口令和POP3服务器地址:
+		email = email # input('Email: ')
+		password = password # input('Password: ')
+		pop3_server = pop3_server # input('POP3 server: ') # pop.126.com   pop.163.com
+
+		# 连接到POP3服务器:
+		server = poplib.POP3(pop3_server)
+		
+		# 可以打开或关闭调试信息:
+		server.set_debuglevel(1)
+
+		# 可选:打印POP3服务器的欢迎文字:
+		# print(server.getwelcome().decode('utf-8'))
+
+		# 身份认证:
+		server.user(email)
+		server.pass_(password)
+
+		# stat()返回邮件数量和占用空间:
+		print('Messages: %s. Size: %s' % server.stat())
+		
+		# list()返回所有邮件的编号:
+		resp, mails, octets = server.list()
+		
+		# 可以查看返回的列表类似[b'1 82923', b'2 2184', ...]
+		# print(mails)
+
+		# 获取最新一封邮件, 注意索引号从1开始:
+		index = len(mails) # 总数
+
+		page = (index - limit) if (index - limit)>0 else 0
+		for x in range(index, page ,-1): #循环获取所有邮件
+
+			resp, lines, octets = server.retr(x)
+
+			# lines存储了邮件的原始文本的每一行,
+			# 可以获得整个邮件的原始文本:
+			msg_content = b'\r\n'.join(lines).decode('utf-8')
+			
+			# 稍后解析出邮件:
+			msg = Parser().parsestr(msg_content)
+
+			msgAll.append(print_info(msg))
+
+		# 可以根据邮件索引号直接从服务器删除邮件:
+		# server.dele(index)
+		# 关闭连接:
+		server.quit()
+	except:
+		pass
+
+	return msgAll
+
+'''
+只需要一行代码就可以把邮件内容解析为Message对象：
+
+msg = Parser().parsestr(msg_content)
+但是这个Message对象本身可能是一个MIMEMultipart对象，即包含嵌套的其他MIMEBase对象，嵌套可能还不止一层。
+
+所以我们要递归地打印出Message对象的层次结构：
+'''
+# indent用于缩进显示:
+def print_info(msg, indent=0):
+	data = {}
+	if indent == 0:
+		for header in ['From', 'To', 'Subject']:
+			value = msg.get(header, '')
+			if value:
+				if header=='Subject':
+					value = decode_str(value)
+
+					data['Subject'] = value #标题
+				else:
+					hdr, addr = parseaddr(value)
+					name = decode_str(hdr)
+					value = u'%s <%s>' % (name, addr)
+
+					data[header] = {'name':name, 'addr':addr} # 发送人、接收人
+
+				print('%s%s: %s' % ('  ' * indent, header, value))
+	
+	if (msg.is_multipart()):
+		parts = msg.get_payload()
+		for n, part in enumerate(parts):
+			print('%spart %s' % ('  ' * indent, n))
+			print('%s--------------------' % ('  ' * indent))
+			print_info(part, indent + 1)
+	else:
+		
+		content_type = msg.get_content_type()
+		if content_type=='text/plain' or content_type=='text/html':
+			# 内容
+			content = msg.get_payload(decode=True)
+			charset = guess_charset(msg)
+			if charset:
+				content = content.decode(charset)
+
+			data['test'] = content
+			# print('%sText: %s' % ('  ' * indent, content + '...'))
+		else:
+			# 附件
+			print('%sAttachment: %s' % ('  ' * indent, content_type))
+
+	return data
+
+#邮件的Subject或者Email中包含的名字都是经过编码后的str，要正常显示，就必须decode：
+def decode_str(s):
+    value, charset = decode_header(s)[0]
+    if charset:
+        value = value.decode(charset)
+    return value
+
+'''
+decode_header()返回一个list，因为像Cc、Bcc这样的字段可能包含多个邮件地址，所以解析出来的会有多个元素。上面的代码我们偷了个懒，只取了第一个元素。
+
+文本邮件的内容也是str，还需要检测编码，否则，非UTF-8编码的邮件都无法正常显示：
+'''
+def guess_charset(msg):
+    # 先从msg对象获取编码:
+    charset = msg.get_charset()
+    if charset is None:
+        content_type = msg.get('Content-Type', '').lower()
+        pos = content_type.find('charset=')
+        if pos >= 0:
+            charset = content_type[pos + 8:].strip()
+    return charset
+
+
+if __name__ == "__main__":
+
+	file1 = 'mail.txt' # 需要检测的文件
+	file2 = 'check_mail.txt' # 结果文件
+
+	file = open("%s/%s" % (dir_, file1))
+	sk = 0
+	while 1:
+		sk += 1 
+		print("执行：%s" % sk)
+		line = file.readline()
+		if not line:
+			break
+
+		# print("=%s=%s=" %(line.split('&')[0].strip(), line.split('&')[1].strip()))
+		check = 0
+		msg = get_mail(line.split('&')[0].strip(), line.split('&')[1].strip(), 50)
+		for i in msg:
+			if ('From' in i) and i['From']['addr'] == 'appleid@id.apple.com':
+				check = 1
+		if check==0:
+			f = open('%s/%s' % (dir_, file2),'a')
+			f.write(line)
+			f.close()
+	    
+			
+
+
+
+
+
+
